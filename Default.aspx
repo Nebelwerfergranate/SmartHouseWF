25482fc911fec759b2999e67501d622ea353069f
Nebelwerfergranate <acht-acht@mail.ru>
2015-11-03 14:47:06 +0200

    Динамическое добавление элементов.

---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Panel ID="Panel1" runat="server">
+        </asp:Panel>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="1" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="2" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="3" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        List<int> panelInfo = (List<int>)Session["Panel"];
+        foreach (int elTypeId in panelInfo)
+        {
+            switch (elTypeId)
+            {
+                case 1: Panel1.Controls.Add(new RadioButton());
+                break;
+                case 2: Panel1.Controls.Add(new CheckBox());
+                break;
+                case 3: Panel1.Controls.Add(new TextBox());
+                break;
+            }
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        ((List<int>)Session["Panel"]).Add(1);
+        Panel1.Controls.Add(new RadioButton());
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        ((List<int>)Session["Panel"]).Add(2);
+        Panel1.Controls.Add(new CheckBox());
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        ((List<int>)Session["Panel"]).Add(3);
+        Panel1.Controls.Add(new TextBox());
+    }
+}
